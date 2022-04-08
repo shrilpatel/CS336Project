3ad48f18889aa24d07031b505a5e9d772aa1ff33
shrilpatel <81841435+shrilpatel@users.noreply.github.com>
2022-04-08 01:00:43 -0400

    Add files via upload

---
+++ ProjectSQL.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS `cs336project`;
+USE `cs336project`;
+
+
+DROP TABLE IF EXISTS `userLogin`;
+
+CREATE TABLE `userLogin`(`email` varchar(50) NOT NULL DEFAULT '', `password` varchar(50) NOT NULL DEFAULT '', `first` varchar(50) DEFAULT NULL,
+	`last` varchar(50) DEFAULT NULL, `userName` varchar(50) DEFAULT NULL, PRIMARY KEY(`email`, `password`))ENGINE=InnoDB DEFAULT CHARSET=latin1;
+    
